4ea72196acc10a72a70acab7dc5bea55c6a809b3
demon-monk <antdude@126.com>
2019-01-07 21:55:31 +0800

    ✨ Feat: install webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,10 @@ module.exports = {
       title: 'webpack demo',
     }),
   ],
+  devServer: {
+    stats: 'errors-only',
+    host: process.env.HOST,
+    port: process.env.PORT,
+    open: true,
+  },
 }
